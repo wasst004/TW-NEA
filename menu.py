135b62c2b5199a5c4adff6640b1f07e87ee9e931
wasst004 <wasst004.309@alexandrapark.school>
2025-01-23 09:57:33 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,7 +4,11 @@ import functions
 def menu(screen, buttons):
     for button in buttons:
         button.draw()
+        complete = button.clicked()
     functions.display_text(screen, "PUTTZONE", 390, 50, 150)
 
     pygame.draw.polygon(screen, "#273963", [(610, 485), (690,535), (610, 585)], 0)
     pygame.draw.polygon(screen, "#86A8F4", [(610, 485), (690, 535), (610, 585)], 1)
+    
+    return complete
+    
