cec30a80ad5dea7457211410257236619603ad9e
wasst004 <wasst004.309@alexandrapark.school>
2025-01-23 07:40:54 +0000

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,10 @@
+import pygame
+import functions
+
+def menu(screen, buttons):
+    for button in buttons:
+        button.draw()
+    functions.display_text(screen, "PUTTZONE", 390, 50, 150)
+
+    pygame.draw.polygon(screen, "#273963", [(610, 485), (690,535), (610, 585)], 0)
+    pygame.draw.polygon(screen, "#86A8F4", [(610, 485), (690, 535), (610, 585)], 1)
