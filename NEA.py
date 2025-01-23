e68d8c0c1f9db05df12e7c072609909d81780193
wasst004 <wasst004.309@alexandrapark.school>
2025-01-23 07:38:15 +0000

    NEA.py
    23/01/25

--- NEA.py
+++ NEA.py
@@ -6,29 +6,36 @@ from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT
 from classes import *
 import functions
 from levelOne import level_1
+from menu import menu
 
 #setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 680))
 clock = pygame.time.Clock()
-framerate = 30
+framerate = 60
 #setting variables
 shotCount = 0
 pars = [2]
 currentLevel = 1
 scoreMessages = ["Par", "Bogey", "Double Bogey", "Triple Bogey", "Eagle", "Birdie"]
-level1 = True
+finished = True
 
 #initialising instances of ball and wall classes
 ball = Ball(screen, 630, 550)
 walls = [
-    Wall(screen, (530,90), (540,90), (540,590), (530,590)),
-    Wall(screen, (710,90), (720,90), (720,590), (710,590)),
-    Wall(screen, (530,80), (720,80), (720,90), (530,90)),
-    Wall(screen, (530,590), (720,590), (720,600), (530,600))
+    Wall(screen, (530,90), (540,90), (540,590), (530,590), "west"),
+    Wall(screen, (710,90), (720,90), (720,590), (710,590), "east"),
+    Wall(screen, (530,80), (720,80), (720,90), (530,90), "north"),
+    Wall(screen, (530,590), (720,590), (720,600), (530,600), "south")
         ]
 hole = Hole(screen, 630, 130)
 
+buttons = [
+    Button(screen, 560, 475, "#40A15D", 160, 120, "square"),
+    Button(screen, 282.5, 535, "#40A15D", 140, 100, "circle"),
+    Button(screen, 922.5, 480, "#40A15D", 120, 100, "square")
+    
+    ]
 
 #main game
 while True:
@@ -36,30 +43,32 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
         
-        #drawing screen
-        screen.fill('#55CE79')
+    #drawing screen
+    screen.fill('#55CE79')
             
-        #manual controls
-        keys = pygame.key.get_pressed()
-        if keys[K_UP]:
-            ball.set_pos(0,-10)
-        elif keys[K_DOWN]:
-            ball.set_pos(0,10)
-        elif keys[K_LEFT]:
-            ball.set_pos(-10,0)
-        elif keys[K_RIGHT]:
-            ball.set_pos(10,0)
+    #manual controls
+    keys = pygame.key.get_pressed()
+    if keys[K_UP]:
+        ball.set_pos(0,-10)
+    elif keys[K_DOWN]:
+        ball.set_pos(0,10)
+    elif keys[K_LEFT]:
+        ball.set_pos(-10,0)
+    elif keys[K_RIGHT]:
+        ball.set_pos(10,0)
                 
-        if level1:
-            #running code for level 1
-            level1, shotCount = level_1(screen, ball, walls, hole, shotCount, currentLevel, pars, scoreMessages)            
+    menu(screen, buttons)    
+
+    # if finished:
+    #     #running code for level 1
+    #     finished, shotCount = level_1(screen, ball, walls, hole, shotCount, currentLevel, pars, scoreMessages)            
             
-        else:
-            #ending screen
-            functions.display_text(screen, "YOU WON LEVEL 1", 600, 400, 80)
-            shotDisplay = functions.calculate_Score(shotCount, scoreMessages, pars, currentLevel)
-            functions.display_text(screen, shotDisplay, 600, 450, 70)
-            #currentLevel += 1
+    # else:
+    #     #ending screen
+    #     functions.display_text(screen, "YOU WON LEVEL 1", 600, 400, 80)
+    #     shotDisplay = functions.calculate_Score(shotCount, scoreMessages, pars, currentLevel)
+    #     functions.display_text(screen, shotDisplay, 600, 450, 70)
+    #     #currentLevel += 1
 
     pygame.display.flip()
     clock.tick(framerate)
