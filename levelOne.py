37b1184f4d5fd9145a26bf3812c0007b0a83b09b
wasst004 <wasst004.309@alexandrapark.school>
2025-01-24 15:02:14 +0000

    Update levelOne.py

--- levelOne.py
+++ levelOne.py
@@ -2,11 +2,11 @@ import pygame
 import functions
 
 
-def level_1(screen, ball, walls, hole, shotCount, currentLevel, pars, scoreMessages):
+def level(screen, ball, walls, hole, shotCount, currentLevel, pars, scoreMessages, rect):
     finished = True
 
     #displays elements of level
-    functions.draw_elements(screen, ball, walls, hole)
+    functions.draw_elements(screen, ball, walls, hole, rect)
     functions.draw_board(screen, currentLevel, shotCount, pars)
             
     #calling ball methods in main code
@@ -14,7 +14,7 @@ def level_1(screen, ball, walls, hole, shotCount, currentLevel, pars, scoreMessa
     ball.aim()
     ball.move()
     ball.collision(walls)
-    finished = hole.collision(ball.get_pos(), finished)
+    finished = hole.collision(ball.get_pos())
 
     #returning useful values to main file
     return finished, shotCount
